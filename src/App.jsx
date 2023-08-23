5741a6f20ca70c1f8e5ba5910035f14ee12b5ad3
joseRonald232001 <phjronald@gmail.com>
2023-08-22 21:57:49 -0400

    instalacion de bibliotecas a usar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+  
+  <h1 className='font-extrabold text-lg text-center p-10'>hello world</h1>
+
+    </>
+  )
+}
+
+export default App
